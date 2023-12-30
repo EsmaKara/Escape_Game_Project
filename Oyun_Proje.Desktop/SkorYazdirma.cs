888c5dbf537fb001c41f8a5a071b9d000ec3fb80
Esma Kara <esma_._kara@hotmail.com>
2023-12-31 00:57:44 +0300

    dosyaya yazdırmayı denedim ama dosyaya erişilemiyor, ama kodun mantığı doğru bence

--- Oyun_Proje.Desktop/SkorYazdirma.cs
+++ Oyun_Proje.Desktop/SkorYazdirma.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Oyun_Proje.Desktop
@@ -9,9 +10,10 @@ namespace Oyun_Proje.Desktop
     internal class SkorYazdirma
     {
         public static List<Label> labelListe = new List<Label>();
+        private static StreamWriter sw = new StreamWriter("D:\\Kodlar\\CSharp\\Oyun_Proje\\Oyun_Proje.Desktop\\Top5.txt", false, Encoding.ASCII);
+        private static StreamReader sr = new StreamReader("D:\\Kodlar\\CSharp\\Oyun_Proje\\Oyun_Proje.Desktop\\Top5.txt");
 
-        private static Label[] lblAd = new Label[5];
-        private static Label[] lblPuan = new Label[5];
+        private static Label[] lblSira = new Label[5];
 
         private static string[] isimDizisi = new string[5];
         private static int[] puanDizisi = { 0, 0, 0, 0, 0 };
@@ -20,12 +22,10 @@ namespace Oyun_Proje.Desktop
         {
             for (int i = 0; i < 5; i++)
             {
-                lblAd[i] = new Label();
-                lblPuan[i] = new Label();   
-                lblAd[i].Location = new Point(20, 60);
-                lblAd[i].ForeColor = Color.Black;
-                lblPuan[i].Location = new Point(210, 60);
-                lblPuan[i].ForeColor = Color.Black;
+                lblSira[i] = new Label();   
+                lblSira[i].Location = new Point(20, 60);
+                lblSira[i].ForeColor = Color.Black;
+                lblSira[i].Size = new Size(70, 100);
             }
             
 
@@ -42,13 +42,13 @@ namespace Oyun_Proje.Desktop
             {
                 if (isimDizisi[j] != null)
                 {
-                    lblAd[j].Text = (j + 1) + ")           " + isimDizisi[j] + "\n";
-                    lblPuan[j].Text = Convert.ToString(puanDizisi[j]) + "\n";
+                    sw.Write((j + 1) + ")           " + isimDizisi[j] + "\t \t \t" + Convert.ToString(puanDizisi[j]) + "\n");
+                    lblSira[j].Text = sr.ReadLine();
 
-                    labelListe.Add(lblAd[j]);
-                    labelListe.Add(lblPuan[j]);
+                    labelListe.Add(lblSira[j]);
                 }
             }
+            sw.Close();
 
             return labelListe;
         }
